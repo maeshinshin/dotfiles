0c3c9d747bda47f30ce76b0b22e7fc5042c1b1f8
maesh <maesh.esh.sh@gmail.com>
2025-04-13 01:02:28 +0900

    add aqua path

--- modules/home-manager/programinglang/go/default.nix
+++ modules/home-manager/programinglang/go/default.nix
@@ -7,6 +7,9 @@
     bash = {
       bashrcExtra = ''
         export PATH=$(go env GOPATH)/bin:$PATH
+        if command -v aqua &> /dev/null; then
+          export PATH=$(aqua root-dir)/bin:$PATH
+        fi
       '';
     };
   };
