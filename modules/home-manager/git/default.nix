61400c67862a3c0014e558e6205884f378395c7e
maesh <106868314+maeshinshin@users.noreply.github.com>
2026-06-17 01:30:46 +0900

    add gpgsign

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -4,14 +4,18 @@
       enable = true;
       userName = "maesh";
       userEmail = "106868314+maeshinshin@users.noreply.github.com";
+      signing = {
+        key = "902E2DB2944937CF79474A78C15D410BFF4995F7";
+        signByDefault = true;
+      };
       delta = {
         enable = true;
       };
-        extraConfig = {
-          core = {
-            editor = "nvim";
-          };
+      extraConfig = {
+        core = {
+          editor = "nvim";
         };
       };
     };
-  }
+  };
+}
